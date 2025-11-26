73a2a247e49aa5c7277d2639a441af9db95a17b7
feralaibot <clingyai@yahoo.com>
2025-11-26 12:51:03 -0700

    Delete glitch.js

--- glitch.js
+++
@@ -1,7 +0,0 @@
-
-window.onload = function () {
-    setTimeout(() => {
-        document.querySelector('.terminal').style.display = 'none';
-        document.querySelector('.content').classList.remove('hidden');
-    }, 4000);
-};
